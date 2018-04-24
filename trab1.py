719909444df05b10933a53a4eae6bf9cf8990aa5
andreus89 <33923525+andreus89@users.noreply.github.com>
2018-04-24 15:30:08 -0300

    Update trab1.py

--- trab1.py
+++ trab1.py
@@ -1,8 +1,23 @@
-keys=['Nome Comercial','Composto principal','Fórmula Química']
-v=[0,0,0]
-for i in range(1,5):
-    v[i]=input('Nome Comercial? ')
-    v[i]=input('Composto principal? ')
-    v[i]=input('Fórmula Química? ')
-    i = dict(zip(keys, v))
-    print(i)
+med_dic={1:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reação adversa mais comuns':0},
+2:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reações adversa mais comuns':0},
+3:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reações adversa mais comuns':0},
+4:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reações adversa mais comuns':0},
+5:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reações adversa mais comuns':0},
+6:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reações adversa mais comuns':0},
+7:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reações adversa mais comuns':0},
+8:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reações adversa mais comuns':0},
+9:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reações adversa mais comuns':0},
+10:{'Nome Comercial':0,'Composto principal':0,'Fórmula Química':0,'Meia-vida de eliminação':0,'Número de comprimidos por embalagem':0,'Preço da embalagem em 3 fornecedores':0,'Quantidade do composto principal por comprimido':0,'Dose diária para um adulto':0,'As 3 reações adversa mais comuns':0}
+}
+
+for i in range(1,10):
+    med_dic[i]['Nome Comercial']=input('Nome Comercial?')
+    med_dic[i]['Composto principal']=input('Composto principal?')
+    med_dic[i]['Fórmula Química']=input('Fórmula Química?')
+    med_dic[i]['Meia-vida de eliminação']=input('Meia-vida de eliminação?')
+    med_dic[i]['Número de comprimidos por embalagem']=input('Número de comprimidos por embalagem?')
+    med_dic[i]['Preço da embalagem em 3 fornecedores']=input('Preço da embalagem em 3 fornecedores?')
+    med_dic[i]['Quantidade do composto principal por comprimido']=input('Quantidade do composto principal por comprimido?')
+    med_dic[i]['Dose diária para um adulto']=input('Dose diária para um adulto?')
+    med_dic[i]['As 3 reações adversa mais comuns']=input('As 3 reações adversa mais comuns?')
+    print(med_dic[i])
